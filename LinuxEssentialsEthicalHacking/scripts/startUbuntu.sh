276566dbe853e59367b2514d66846c5b0539e668
ZapRowsdower <ely.jonathan@gmail.com>
2021-09-22 11:33:50 +0000

    Better docker image building+script

--- LinuxEssentialsEthicalHacking/scripts/startUbuntu.sh
+++ LinuxEssentialsEthicalHacking/scripts/startUbuntu.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 export CONTAINER_NAME=ubuntu-instance
-docker run -td --name $CONTAINER_NAME ubuntu &&
+# Go to Dockerfile dir
+cd .. &&
+# Build from the docker file at that dir
+# Tag the image as 'my-ubuntu'
+docker image build . -t my-ubuntu
+# Run the built image, name it
+docker run -td --name $CONTAINER_NAME my-ubuntu &&
+# Shell into the running docker container
 docker exec -it $CONTAINER_NAME bash
