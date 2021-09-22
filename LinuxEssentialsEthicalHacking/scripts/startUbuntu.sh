f9adcfd238bd865049845d4640f0f22d9bdabca1
ZapRowsdower <ely.jonathan@gmail.com>
2021-09-22 11:38:53 +0000

    Rm unecessary cd from script

--- LinuxEssentialsEthicalHacking/scripts/startUbuntu.sh
+++ LinuxEssentialsEthicalHacking/scripts/startUbuntu.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 export CONTAINER_NAME=ubuntu-instance
-# Go to Dockerfile dir
-cd .. &&
-# Build from the docker file at that dir
-# Tag the image as 'my-ubuntu'
+# Build image from docker file. Tag the image as 'my-ubuntu'
 docker image build . -t my-ubuntu
 # Run the built image, name it
 docker run -td --name $CONTAINER_NAME my-ubuntu &&
